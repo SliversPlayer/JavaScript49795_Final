ec5a23063ca7f966d2eebd8ed898129f3807f779
Roberto Méndez Strobl <rmendezstrobl@gmail.com>
2023-12-10 20:17:46 -0300

    Avance, creación de HTML form, e implementación de arrays

--- SimuladorRendiciones.js
+++ SimuladorRendiciones.js
@@ -28,9 +28,7 @@
 // Identificación de la rendición
 
 let tipoRendicion = 0;
-let numTarjeta = 0;
-let porRendir = 0;
-let continuar;
+const infoRendicion = [];
 
 //Rendicion
 
@@ -40,40 +38,36 @@ let tipoDoc;
 let centroCosto;
 let monto;
 let rendicion = [];
-let continuarRendicion;
 let totalMonto = 0;
-
-// Función para definir el tipo de rendición
-function funcTipo(tipoRendicion) {
-    switch (tipoRendicion) {
-        case 1:
-            return `Fondo Fijo`;
-            break;
-        case 2:
-            return `Fondo por Rendir`;
-            break;
-        case 3:
-            return `Tarjeta de Crédito`;
-            break;
-        default:
-            return "Tipo incorrecto"
-    }
-}
-
+let porRendir;
+let numTarjeta;
 
 function iniciarRendicion() {
     const nombre = document.getElementById("nombre").value;
     const id = document.getElementById("id").value;
     const fecha = document.getElementById("fecha").value;
-    const tipoRendicion = parseInt(document.getElementById("tipoRendicion").value);
+    const tipoRendicion = document.getElementById("tipoRendicion").value;
+    porRendir = document.getElementById("porRendir").value || null;
+    numTarjeta = document.getElementById("numTarjeta").value || null;
 
-    }
+    const datosRendicion = {
+        nombre,
+        id,
+        fecha,
+        tipoRendicion,
+        porRendir,
+        numTarjeta
+};
+    infoRendicion.push(datosRendicion);
+    alert(`Empleado: ${nombre}\n id: ${id}\nFecha Rendición: ${fecha}\nTipo Rendición: ${funcTipo(tipoRendicion)}`);
 
-// alert(`Empleado: ${nombre}\nid: ${id}\nFecha Rendición: ${fecha}\nTipo Rendición: ${funcTipo(tipoRendicion)}`);
+    limpiarFormulario();
+}
+
+// alert(`Empleado: ${nombre}\n id: ${id}\nFecha Rendición: ${fecha}\nTipo Rendición: ${funcTipo(tipoRendicion)}`);
 
 function mostrarNumTarjeta() {
     let tipoRendicion = document.getElementById("tipoRendicion").value;
-
         switch (tipoRendicion) {
             case "1":
                 porRendirContainer.style.display = "none";
@@ -84,52 +78,64 @@ function mostrarNumTarjeta() {
                 numTarjetaContainer.style.display = "none";
                 break;
             case "3":
-                numTarjetaContainer.style.display = "block";
                 porRendirContainer.style.display = "none";
+                numTarjetaContainer.style.display = "block";
                 break;
             default:
-               alert(`Tipo incorrecto`)
+                return "Tipo incorrecto"
                 }
             }
 
 
 // Constructor para representar una rendición
-function Rendicion(fecha, glosa, tipoDoc, centroCosto, monto) {
-    this.fecha = format(new Date(fecha), 'dd-MM-yyyy');
-    this.glosa = glosa;
+function Rendicion(fechaRendicion, glosaRendicion, tipoDoc, centroCosto, monto) {
+    this.fechaRendicion = fechaRendicion;
+    this.glosaRendicion = glosaRendicion;
     this.tipoDoc = tipoDoc;
     this.centroCosto = centroCosto;
     this.monto = monto;
 }
 
 // Función para capturar por prompt datos de rendiciones,
-// queda pendiente separar la impresión del detalle e implementar libreria de fechas
 function obtenerDatosRendicion() {
+    fechaRendicion = document.getElementById("fechaRendicion").value;
+    glosaRendicion = document.getElementById("glosaRendicion").value;
+    tipoDoc = document.getElementById("tipoDoc").value;
+    centroCosto = document.getElementById("centroCosto").value;
+    monto = document.getElementById("monto").value;
 
-    do {
-        fechaRendicion = document.getElementById("fechaRendicion").value;
-        glosaRendicion = document.getElementById("glosaRendicion").value;
-        tipoDoc = document.getElementById("tipoDoc").value;
-        centroCosto = document.getElementById("centroCosto").value;
-        monto = document.getElementById("monto").value;
+    const nuevaRendicion = new Rendicion(fechaRendicion, glosaRendicion, tipoDoc, centroCosto, monto);
+    rendicion.push(nuevaRendicion);
 
-        const nuevaRendicion = new Rendicion(fechaRendicion, glosaRendicion, tipoDoc, centroCosto, monto);
-        rendicion.push(nuevaRendicion);
+    totalMonto += monto;
 
-        totalMonto += monto;
+    alert(`Fecha Rendición: ${nuevaRendicion.fecha}\nDescripción: ${nuevaRendicion.glosa}\nTipo Documento: ${nuevaRendicion.tipoDoc}\nCentro de costo: ${nuevaRendicion.centroCosto}\nMonto: ${nuevaRendicion.monto}\nMonto Total: ${totalMonto.toFixed(0)}`);
+}
 
-        alert(`Fecha Rendición: ${nuevaRendicion.fecha}\nDescripción: ${nuevaRendicion.glosa}\nTipo Documento: ${nuevaRendicion.tipoDoc}\nCentro de costo: ${nuevaRendicion.centroCosto}\nMonto: ${nuevaRendicion.monto}\nMonto Total: ${totalMonto.toFixed(0)}`);
-        
-        continuarRendicion = prompt('¿Desea ingresar otra rendición? si/no');
-        if (continuarRendicion == "no") {
-            console.log('Datos concatenados:', rendicion);
+// Convert the value to string for better identification
+function funcTipo(tipoRendicion) {
+
+
+    switch (tipoRendicion) {
+        case "1":
+            return `Fondo Fijo`;
             break;
-        }
-
-        } while (continuarRendicion.toLowerCase() !== "no");
+        case "2":
+            return `Fondo por Rendir  ${porRendir}`;
+            break;
+        case "3":
+            return `Tarjeta de Crédito ${numTarjeta}`;
+            break;
+        default:
+            return "Tipo incorrecto"
     }
-
-obtenerDatosRendicion();
-
+}
 
 
+// Clean the form
+function limpiarFormulario() {
+    document.getElementById("rendicionForm").reset();
+    // Puedes ocultar los contenedores adicionales aquí si es necesario
+    document.getElementById("porRendirContainer").style.display = "none";
+    document.getElementById("numTarjetaContainer").style.display = "none";
+}
