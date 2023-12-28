6c311f09111d71a65cb7511e04531c43fd997194
Roberto Méndez Strobl <rmendezstrobl@gmail.com>
2023-12-28 00:48:24 -0300

    test

---
+++ pages/generateReport.js
@@ -0,0 +1,8 @@
+
+const almacenamientoLocal = JSON.parse(localStorage.getItem('almacenamientoLocal')) || {};
+
+
+// element reference
+var reporteContainer = document.getElementById("reportContainer");
+
+// generate report
