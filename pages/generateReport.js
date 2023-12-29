7c8f9f82dbc3dd18c5ab66e2a0e47473a47e714c
Roberto Méndez Strobl <rmendezstrobl@gmail.com>
2023-12-28 23:09:56 -0300

    Primera etapa sección reporte

--- pages/generateReport.js
+++ pages/generateReport.js
@@ -1,3 +1,6 @@
+
+const almacenamientoLocal = JSON.parse(localStorage.getItem('almacenamientoLocal')) || {};
+
 // Transform 'almacenamientoLocal' undefined object in string object
 var datosAlmacenados = localStorage.getItem('almacenamientoLocal');
 // Parse converts string type data into array object JS type data
@@ -10,7 +13,7 @@ const params = new URLSearchParams(queryString);
 const idString = params.get('id');
 
 
-/*
+
 // Verify if Local Storage has data in it
 if (datosAlmacenados) {
     // Parse converts string type data into array object JS type data
@@ -23,22 +26,37 @@ if (datosAlmacenados) {
     console.log('No hay datos almacenados en el Local Storage.');
 }
 
-document.getElementById('data-output').innerHTML = arrayDatos.name;
-*/
-localStorage.setItem('miDato', 'Hola, este es mi dato almacenado en localStorage');
+//document.getElementById('data-output').innerHTML = arrayDatos.name;
 
-// Recupera el valor de localStorage
-var miDato = localStorage.getItem('miDato');
+// localStorage.setItem('miDato', 'Hola, este es mi dato almacenado en localStorage');
+
+
+
+
+
+usuarioData = (almacenamientoLocal[idString]);
+nombre = (almacenamientoLocal[idString].nombre);
+fecha = (almacenamientoLocal[idString].fecha);
+tipoRendicion = (almacenamientoLocal[idString].tipoRendicion);
+
+dato = (almacenamientoLocal[idString].rendiciones);
+
+largo = (almacenamientoLocal[idString].rendiciones.length);
+ulti = (dato[largo-1]);
 
 // Muestra el valor en el contenedor HTML
-var miDatoContainer = document.getElementById('miDatoContainer');
+var id = document.getElementById('id');
+id.textContent = idString;
 
+var nombreReport = document.getElementById('nombre');
+nombreReport.textContent = nombre;
 
-miDatoContainer.textContent = datosAlmacenados;
+var fechaReport = document.getElementById('fecha');
+fechaReport.textContent = fecha;
 
-dato = (datosAlmacenados[idString].rendiciones);
-/*
-largo = (datosAlmacenados[idString].rendiciones.length);
-ulti = (dato[largo-1]);
-*/
+var tipoRendicionReport = document.getElementById('tipoRendicion');
+tipoRendicionReport.textContent = tipoRendicion;
+
+// Como revisar el array dentro del objeto local storage
+// almacenamientoLocal[idString].rendiciones[3]
 
